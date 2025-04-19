6eed3b5df204e7c9d21c860180fac13114bd0198
dxlliv <hello@dxlliv.dev>
2025-04-19 21:59:20 +0200

    Minor improvements

--- runtime/plugin/index.ts
+++ runtime/plugin/index.ts
@@ -1,9 +1,9 @@
 import {defineNuxtPlugin} from "nuxt/app"
 import {defineDesktopApp} from "@owdproject/core/runtime/utils/utilsDesktop"
-import AppAbout from '../../owd.config'
+import configAppAbout from '../../owd.config'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook('app:created', async () => {
-        await defineDesktopApp(AppAbout)
+        await defineDesktopApp(configAppAbout)
     })
 })
